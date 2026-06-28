aa36d7e5916c53dba406438287aea9b46430ae86
Manvik sagar <75376796+manviksagar6546@users.noreply.github.com>
2026-06-28 16:35:40 +0530

    Delete Quiz directory

--- Quiz/src/Main.java
+++
@@ -1,8 +0,0 @@
-class Main{
-    public static void main(String[] args){
-//        System.out.println("Lets create new Journy of SUCCESS");
-
-        QuestionService service = new QuestionService();
-        service.playQuiz();
-    }
-}
